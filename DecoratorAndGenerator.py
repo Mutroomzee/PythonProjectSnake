5c4b9e4b6ab8b1800fcaf962f5d9cedb47906d31
Mutroomzee <akkpb00@gmail.com>
2025-11-23 18:40:17 +0700

    testingcode

---
+++ DecoratorAndGenerator.py
@@ -0,0 +1,15 @@
+
+def my_decorator(func):
+  def wrapper():
+    print('Before function execution')
+    func()
+    print('After function execution')
+  return wrapper
+
+
+@my_decorator
+def say_hello():
+    print('Hello, decorators!')
+
+# Call the decorated function
+say_hello()
